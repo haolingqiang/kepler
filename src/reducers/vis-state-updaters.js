keplergl/kepler.gl

[Feat] Add trip layer - 1 (#632) (#632)
* duplicate geoJson layer as trip canvas

* remove dataset from reducer param

* remove stroke control

src/reducers/vis-state-updaters.js
@@ -30,10 +30,7 @@ import {loadFilesErr} from 'actions/vis-state-actions';
 import {addDataToMap} from 'actions';
 
 // Utils
-import {
-  getDefaultInteraction,
-  findFieldsToShow
-} from 'utils/interaction-utils';
+import {getDefaultInteraction, findFieldsToShow} from 'utils/interaction-utils';
 import {
   getDefaultFilter,
   getFilterProps,
@@ -45,7 +42,8 @@ import {createNewDataEntry} from 'utils/dataset-utils';
 
 import {
   findDefaultLayer,
-  calculateLayerData
+  calculateLayerData,
+  getTimeAnimationDomain
 } from 'utils/layer-utils/layer-utils';
 
 import {
@@ -165,7 +163,17 @@ export const INITIAL_VIS_STATE = {
   ],
 
   // defaults layer classes
-  layerClasses: LayerClasses
+  layerClasses: LayerClasses,
+
+  // default animation
+  animationConfig: {
+    domain: [0, 2000],
+    currentTime: 0,
+    duration: 10
+  }
+  // layers: {
+  //   [layer.id]: {enabled: true, speed: 1000, currentTime: null}
+  // }
 };
 
 function updateStateWithLayerAndData(state, {layerData, layer, idx}) {
@@ -178,15 +186,15 @@ function updateStateWithLayerAndData(state, {layerData, layer, idx}) {
   };
 }
 
- /**
-  * Update layer base config: dataId, label, column, isVisible
-  * @memberof visStateUpdaters
-  * @param {Object} state `visState`
-  * @param {Object} action action
-  * @param {Object} action.oldLayer layer to be updated
-  * @param {Object} action.newConfig new config
-  * @returns {Object} nextState
-  */
+/**
+ * Update layer base config: dataId, label, column, isVisible
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @param {Object} action.oldLayer layer to be updated
+ * @param {Object} action.newConfig new config
+ * @returns {Object} nextState
+ */
 export function layerConfigChangeUpdater(state, action) {
   const {oldLayer} = action;
   const idx = state.layers.findIndex(l => l.id === oldLayer.id);
@@ -194,12 +202,9 @@ export function layerConfigChangeUpdater(state, action) {
   const newLayer = oldLayer.updateLayerConfig(action.newConfig);
   if (newLayer.shouldCalculateLayerData(props)) {
     const oldLayerData = state.layerData[idx];
-    const {layerData, layer} = calculateLayerData(
-      newLayer,
-      state,
-      oldLayerData,
-      {sameData: true}
-    );
+    const {layerData, layer} = calculateLayerData(newLayer, state, oldLayerData, {
+      sameData: true
+    });
     return updateStateWithLayerAndData(state, {layerData, layer, idx});
   }
 
@@ -207,9 +212,9 @@ export function layerConfigChangeUpdater(state, action) {
   if ('isVisible' in action.newConfig && state.splitMaps.length) {
     newState = {
       ...state,
-      splitMaps: action.newConfig.isVisible ?
-        addNewLayersToSplitMap(state.splitMaps, newLayer) :
-        removeLayerFromSplitMaps(state.splitMaps, newLayer)
+      splitMaps: action.newConfig.isVisible
+        ? addNewLayersToSplitMap(state.splitMaps, newLayer)
+        : removeLayerFromSplitMaps(state.splitMaps, newLayer)
     };
   }
 
@@ -219,19 +224,24 @@ export function layerConfigChangeUpdater(state, action) {
 function addOrRemoveTextLabels(newFields, textLabel) {
   let newTextLabel = textLabel.slice();
 
-  const currentFields = textLabel.map(tl => tl.field && tl.field.name).filter(d => d);
+  const currentFields = textLabel
+    .map(tl => tl.field && tl.field.name)
+    .filter(d => d);
 
   const addFields = newFields.filter(f => !currentFields.includes(f.name));
-  const deleteFields = currentFields
-    .filter(f => !newFields.find(fd => fd.name === f));
+  const deleteFields = currentFields.filter(
+    f => !newFields.find(fd => fd.name === f)
+  );
 
   // delete
-  newTextLabel = newTextLabel.filter(tl => tl.field && !deleteFields.includes(tl.field.name));
+  newTextLabel = newTextLabel.filter(
+    tl => tl.field && !deleteFields.includes(tl.field.name)
+  );
   newTextLabel = !newTextLabel.length ? [DEFAULT_TEXT_LABEL] : newTextLabel;
 
   // add
   newTextLabel = [
-    ...(newTextLabel.filter(tl => tl.field)),
+    ...newTextLabel.filter(tl => tl.field),
     ...addFields.map(af => ({
       ...DEFAULT_TEXT_LABEL,
       field: af
@@ -246,9 +256,9 @@ function updateTextLabelPropAndValue(idx, prop, value, textLabel) {
 
   if (prop && (value || textLabel.length === 1)) {
     newTextLabel = textLabel.map((tl, i) =>
-      i === idx ? {...tl, [prop]: value} : tl);
+      i === idx ? {...tl, [prop]: value} : tl
+    );
   } else if (prop === 'field' && value === null && textLabel.length > 1) {
-
     // remove label when field value is set to null
     newTextLabel.splice(idx, 1);
   }
@@ -268,16 +278,16 @@ export function layerTextLabelChangeUpdater(state, action) {
 
   // if idx is set to length, add empty text label
   if (!textLabel[idx] && idx === textLabel.length) {
-    newTextLabel = [
-      ...textLabel,
-      DEFAULT_TEXT_LABEL
-    ];
+    newTextLabel = [...textLabel, DEFAULT_TEXT_LABEL];
   }
 
   // update text label prop and value
   newTextLabel = updateTextLabelPropAndValue(idx, prop, value, newTextLabel);
 
-  return layerConfigChangeUpdater(state, {oldLayer, newConfig: {textLabel: newTextLabel}});
+  return layerConfigChangeUpdater(state, {
+    oldLayer,
+    newConfig: {textLabel: newTextLabel}
+  });
 }
 
 /**
@@ -322,13 +332,15 @@ export function layerTypeChangeUpdater(state, action) {
       ...state,
       splitMaps: state.splitMaps.map(settings => {
         const {[oldId]: oldLayerMap, ...otherLayers} = settings.layers;
-        return oldId in settings.layers ? {
-          ...settings,
-          layers: {
-            ...otherLayers,
-            [layer.id]: oldLayerMap
-          }
-        } : settings;
+        return oldId in settings.layers
+          ? {
+              ...settings,
+              layers: {
+                ...otherLayers,
+                [layer.id]: oldLayerMap
+              }
+            }
+          : settings;
       })
     };
   }
@@ -388,12 +400,9 @@ export function layerVisConfigChangeUpdater(state, action) {
 
   if (newLayer.shouldCalculateLayerData(props)) {
     const oldLayerData = state.layerData[idx];
-    const {layerData, layer} = calculateLayerData(
-      newLayer,
-      state,
-      oldLayerData,
-      {sameData: true}
-    );
+    const {layerData, layer} = calculateLayerData(newLayer, state, oldLayerData, {
+      sameData: true
+    });
     return updateStateWithLayerAndData(state, {layerData, layer, idx});
   }
 
@@ -423,7 +432,11 @@ export function interactionConfigChangeUpdater(state, action) {
   // but coordinates can be shown at all time
   const contradict = ['brush', 'tooltip'];
 
-  if (contradict.includes(config.id) && config.enabled && !state.interactionConfig[config.id].enabled) {
+  if (
+    contradict.includes(config.id) &&
+    config.enabled &&
+    !state.interactionConfig[config.id].enabled
+  ) {
     // only enable one interaction at a time
     contradict.forEach(k => {
       if (k !== config.id) {
@@ -598,8 +611,8 @@ export const addFilterUpdater = (state, action) =>
  */
 export const toggleFilterAnimationUpdater = (state, action) => ({
   ...state,
-  filters: state.filters.map(
-    (f, i) => (i === action.idx ? {...f, isAnimating: !f.isAnimating} : f)
+  filters: state.filters.map((f, i) =>
+    i === action.idx ? {...f, isAnimating: !f.isAnimating} : f
   )
 });
 
@@ -615,10 +628,52 @@ export const toggleFilterAnimationUpdater = (state, action) => ({
  */
 export const updateAnimationSpeedUpdater = (state, action) => ({
   ...state,
-  filters: state.filters.map(
-    (f, i) => (i === action.idx ? {...f, speed: action.speed} : f)
+  filters: state.filters.map((f, i) =>
+    i === action.idx ? {...f, speed: action.speed} : f
   )
 });
+
+/**
+ * Update animation current time
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @param {Number} action.value current time value of action
+ * @returns {Object} nextState
+ * @public
+ *
+ */
+
+export const playAnimationUpdater = (state, {value}) => ({
+  ...state,
+  animationConfig: {
+    ...state.animationConfig,
+    currentTime: value
+  }
+});
+
+/**
+ * Enable animation domain with the min and max of timestamps from geojson
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @returns {Object} nextState
+ * @public
+ *
+ */
+
+export const enableLayerAnimationUpdater = (state, action) => {
+  const {oldLayer} = action;
+  const [minTs, maxTs] = getTimeAnimationDomain(oldLayer, state.datasets);
+  return {
+    ...state,
+    animationConfig: {
+      ...state.animationConfig,
+      currentTime: minTs,
+      domain: [minTs, maxTs]
+    }
+  };
+};
 
 /**
  * Show larger time filter at bottom for time playback (apply to time filter only)
@@ -846,7 +901,7 @@ export const showDatasetTableUpdater = (state, action) => {
  * @returns {Object} nextState
  * @public
  */
-export const resetMapConfigVisStateUpdater = (state) => ({
+export const resetMapConfigVisStateUpdater = state => ({
   ...INITIAL_VIS_STATE,
   ...state.initialState,
   initialState: state.initialState
@@ -914,10 +969,12 @@ export const layerHoverUpdater = (state, action) => ({
  */
 export const layerClickUpdater = (state, action) => ({
   ...state,
-  mousePos: state.interactionConfig.coordinate.enabled ? {
-    ...state.mousePos,
-    pinned: state.mousePos.pinned ? null : cloneDeep(state.mousePos)
-  } : state.mousePos,
+  mousePos: state.interactionConfig.coordinate.enabled
+    ? {
+        ...state.mousePos,
+        pinned: state.mousePos.pinned ? null : cloneDeep(state.mousePos)
+      }
+    : state.mousePos,
   clicked: action.info && action.info.picked ? action.info : null
 });
 
@@ -928,15 +985,14 @@ export const layerClickUpdater = (state, action) => ({
  * @returns {Object} nextState
  * @public
  */
-export const mapClickUpdater = (state) => {
+export const mapClickUpdater = state => {
   return {
-  ...state,
-  clicked: null
-}
+    ...state,
+    clicked: null
+  };
 };
 
 export const mouseMoveUpdater = (state, {evt}) => {
-
   if (Object.values(state.interactionConfig).some(config => config.enabled)) {
     return {
       ...state,
@@ -949,7 +1005,7 @@ export const mouseMoveUpdater = (state, {evt}) => {
   }
 
   return state;
-}
+};
 /**
  * Toggle visibility of a layer for a split map
  * @memberof visStateUpdaters
@@ -984,14 +1040,18 @@ export const toggleLayerForMapUpdater = (state, {mapIndex, layerId}) => {
 
   return {
     ...state,
-    splitMaps: splitMaps.map((sm, i) => i === mapIndex ? {
-      ...splitMaps[i],
-      layers: {
-        ...splitMaps[i].layers,
-        // if layerId not in layers, set it to visible
-        [layerId]: !splitMaps[i].layers[layerId]
-      }
-    } : sm)
+    splitMaps: splitMaps.map((sm, i) =>
+      i === mapIndex
+        ? {
+            ...splitMaps[i],
+            layers: {
+              ...splitMaps[i].layers,
+              // if layerId not in layers, set it to visible
+              [layerId]: !splitMaps[i].layers[layerId]
+            }
+          }
+        : sm
+    )
   };
 };
 
@@ -1020,7 +1080,6 @@ export const updateVisDataUpdater = (state, action) => {
   const datasets = Array.isArray(action.datasets)
     ? action.datasets
     : [action.datasets];
-
   if (action.config) {
     // apply config if passed from action
     state = receiveMapConfigUpdater(state, {
@@ -1035,7 +1094,6 @@ export const updateVisDataUpdater = (state, action) => {
     }),
     {}
   );
-
   if (!Object.keys(newDateEntries).length) {
     return state;
   }
@@ -1109,10 +1167,7 @@ function computeSplitMapLayers(layers) {
       {}
     );
 
-  return [
-    {layers: mapLayers},
-    {layers: cloneDeep(mapLayers)}
-  ];
+  return [{layers: mapLayers}, {layers: cloneDeep(mapLayers)}];
 }
 
 /**
@@ -1132,7 +1187,7 @@ function removeLayerFromSplitMaps(splitMaps, layer) {
       ...settings,
       layers: newLayers
     };
-  })
+  });
 }
 
 /**
@@ -1156,10 +1211,12 @@ function addNewLayersToSplitMap(splitMaps, layers) {
       ...settings.layers,
       ...newLayers.reduce(
         (accu, newLayer) =>
-          [newLayer.id] in settings.layers || !newLayer.config.isVisible ? accu : {
-            ...accu,
-            [newLayer.id]: newLayer.config.isVisible
-          },
+          [newLayer.id] in settings.layers || !newLayer.config.isVisible
+            ? accu
+            : {
+                ...accu,
+                [newLayer.id]: newLayer.config.isVisible
+              },
         {}
       )
     }
@@ -1183,11 +1240,12 @@ function closeSpecificMapAtIndex(state, action) {
 
   // update layer visibility
   const newLayers = layers.map(layer =>
-    !mapLayers[layer.id] && layer.config.isVisible ?
-      layer.updateLayerConfig({
-        // if layer.id is not in mapLayers, it should be inVisible
-        isVisible: false
-      }) : layer
+    !mapLayers[layer.id] && layer.config.isVisible
+      ? layer.updateLayerConfig({
+          // if layer.id is not in mapLayers, it should be inVisible
+          isVisible: false
+        })
+      : layer
   );
 
   // delete map
@@ -1216,16 +1274,19 @@ export const loadFilesUpdater = (state, action) => {
   const loadFileTasks = [
     Task.all(filesToLoad.map(LOAD_FILE_TASK)).bimap(
       results => {
-        const data = results.reduce((f, c) => ({
-          // using concat here because the current datasets could be an array or a single item
-          datasets: f.datasets.concat(c.datasets),
-          // we need to deep merge this thing unless we find a better solution
-          // this case will only happen if we allow to load multiple keplergl json files
-          config: {
-            ...f.config,
-            ...(c.config || {})
-          }
-        }), {datasets: [], config: {}, options: {centerMap: true}});
+        const data = results.reduce(
+          (f, c) => ({
+            // using concat here because the current datasets could be an array or a single item
+            datasets: f.datasets.concat(c.datasets),
+            // we need to deep merge this thing unless we find a better solution
+            // this case will only happen if we allow to load multiple keplergl json files
+            config: {
+              ...f.config,
+              ...(c.config || {})
+            }
+          }),
+          {datasets: [], config: {}, options: {centerMap: true}}
+        );
         return addDataToMap(data);
       },
       error => loadFilesErr(error)
@@ -1271,6 +1332,7 @@ export function addDefaultLayers(state, datasets) {
     ],
     []
   );
+
   return {
     ...state,
     layers: [...state.layers, ...defaultLayers],

haolingqiang/kepler

Signed-off-by: Shan He <heshan0131@gmail.com>

src/reducers/vis-state-updaters.js
@@ -30,10 +30,7 @@ import {loadFilesErr} from 'actions/vis-state-actions';
 import {addDataToMap} from 'actions';
 
 // Utils
-import {
-  getDefaultInteraction,
-  findFieldsToShow
-} from 'utils/interaction-utils';
+import {getDefaultInteraction, findFieldsToShow} from 'utils/interaction-utils';
 import {
   getDefaultFilter,
   getFilterProps,
@@ -45,7 +42,8 @@ import {createNewDataEntry} from 'utils/dataset-utils';
 
 import {
   findDefaultLayer,
-  calculateLayerData
+  calculateLayerData,
+  getTimeAnimationDomain
 } from 'utils/layer-utils/layer-utils';
 
 import {
@@ -172,7 +170,17 @@ export const INITIAL_VIS_STATE = {
   ],
 
   // defaults layer classes
-  layerClasses: LayerClasses
+  layerClasses: LayerClasses,
+
+  // default animation
+  animationConfig: {
+    domain: [0, 2000],
+    currentTime: 0,
+    duration: 10
+  }
+  // layers: {
+  //   [layer.id]: {enabled: true, speed: 1000, currentTime: null}
+  // }
 };
 
 function updateStateWithLayerAndData(state, {layerData, layer, idx}) {
@@ -185,15 +193,15 @@ function updateStateWithLayerAndData(state, {layerData, layer, idx}) {
   };
 }
 
- /**
-  * Update layer base config: dataId, label, column, isVisible
-  * @memberof visStateUpdaters
-  * @param {Object} state `visState`
-  * @param {Object} action action
-  * @param {Object} action.oldLayer layer to be updated
-  * @param {Object} action.newConfig new config
-  * @returns {Object} nextState
-  */
+/**
+ * Update layer base config: dataId, label, column, isVisible
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @param {Object} action.oldLayer layer to be updated
+ * @param {Object} action.newConfig new config
+ * @returns {Object} nextState
+ */
 export function layerConfigChangeUpdater(state, action) {
   const {oldLayer} = action;
   const idx = state.layers.findIndex(l => l.id === oldLayer.id);
@@ -201,12 +209,9 @@ export function layerConfigChangeUpdater(state, action) {
   const newLayer = oldLayer.updateLayerConfig(action.newConfig);
   if (newLayer.shouldCalculateLayerData(props)) {
     const oldLayerData = state.layerData[idx];
-    const {layerData, layer} = calculateLayerData(
-      newLayer,
-      state,
-      oldLayerData,
-      {sameData: true}
-    );
+    const {layerData, layer} = calculateLayerData(newLayer, state, oldLayerData, {
+      sameData: true
+    });
     return updateStateWithLayerAndData(state, {layerData, layer, idx});
   }
 
@@ -214,9 +219,9 @@ export function layerConfigChangeUpdater(state, action) {
   if ('isVisible' in action.newConfig && state.splitMaps.length) {
     newState = {
       ...state,
-      splitMaps: action.newConfig.isVisible ?
-        addNewLayersToSplitMap(state.splitMaps, newLayer) :
-        removeLayerFromSplitMaps(state.splitMaps, newLayer)
+      splitMaps: action.newConfig.isVisible
+        ? addNewLayersToSplitMap(state.splitMaps, newLayer)
+        : removeLayerFromSplitMaps(state.splitMaps, newLayer)
     };
   }
 
@@ -226,19 +231,24 @@ export function layerConfigChangeUpdater(state, action) {
 function addOrRemoveTextLabels(newFields, textLabel) {
   let newTextLabel = textLabel.slice();
 
-  const currentFields = textLabel.map(tl => tl.field && tl.field.name).filter(d => d);
+  const currentFields = textLabel
+    .map(tl => tl.field && tl.field.name)
+    .filter(d => d);
 
   const addFields = newFields.filter(f => !currentFields.includes(f.name));
-  const deleteFields = currentFields
-    .filter(f => !newFields.find(fd => fd.name === f));
+  const deleteFields = currentFields.filter(
+    f => !newFields.find(fd => fd.name === f)
+  );
 
   // delete
-  newTextLabel = newTextLabel.filter(tl => tl.field && !deleteFields.includes(tl.field.name));
+  newTextLabel = newTextLabel.filter(
+    tl => tl.field && !deleteFields.includes(tl.field.name)
+  );
   newTextLabel = !newTextLabel.length ? [DEFAULT_TEXT_LABEL] : newTextLabel;
 
   // add
   newTextLabel = [
-    ...(newTextLabel.filter(tl => tl.field)),
+    ...newTextLabel.filter(tl => tl.field),
     ...addFields.map(af => ({
       ...DEFAULT_TEXT_LABEL,
       field: af
@@ -253,9 +263,9 @@ function updateTextLabelPropAndValue(idx, prop, value, textLabel) {
 
   if (prop && (value || textLabel.length === 1)) {
     newTextLabel = textLabel.map((tl, i) =>
-      i === idx ? {...tl, [prop]: value} : tl);
+      i === idx ? {...tl, [prop]: value} : tl
+    );
   } else if (prop === 'field' && value === null && textLabel.length > 1) {
-
     // remove label when field value is set to null
     newTextLabel.splice(idx, 1);
   }
@@ -275,16 +285,16 @@ export function layerTextLabelChangeUpdater(state, action) {
 
   // if idx is set to length, add empty text label
   if (!textLabel[idx] && idx === textLabel.length) {
-    newTextLabel = [
-      ...textLabel,
-      DEFAULT_TEXT_LABEL
-    ];
+    newTextLabel = [...textLabel, DEFAULT_TEXT_LABEL];
   }
 
   // update text label prop and value
   newTextLabel = updateTextLabelPropAndValue(idx, prop, value, newTextLabel);
 
-  return layerConfigChangeUpdater(state, {oldLayer, newConfig: {textLabel: newTextLabel}});
+  return layerConfigChangeUpdater(state, {
+    oldLayer,
+    newConfig: {textLabel: newTextLabel}
+  });
 }
 
 /**
@@ -329,13 +339,15 @@ export function layerTypeChangeUpdater(state, action) {
       ...state,
       splitMaps: state.splitMaps.map(settings => {
         const {[oldId]: oldLayerMap, ...otherLayers} = settings.layers;
-        return oldId in settings.layers ? {
-          ...settings,
-          layers: {
-            ...otherLayers,
-            [layer.id]: oldLayerMap
-          }
-        } : settings;
+        return oldId in settings.layers
+          ? {
+              ...settings,
+              layers: {
+                ...otherLayers,
+                [layer.id]: oldLayerMap
+              }
+            }
+          : settings;
       })
     };
   }
@@ -395,12 +407,9 @@ export function layerVisConfigChangeUpdater(state, action) {
 
   if (newLayer.shouldCalculateLayerData(props)) {
     const oldLayerData = state.layerData[idx];
-    const {layerData, layer} = calculateLayerData(
-      newLayer,
-      state,
-      oldLayerData,
-      {sameData: true}
-    );
+    const {layerData, layer} = calculateLayerData(newLayer, state, oldLayerData, {
+      sameData: true
+    });
     return updateStateWithLayerAndData(state, {layerData, layer, idx});
   }
 
@@ -430,7 +439,11 @@ export function interactionConfigChangeUpdater(state, action) {
   // but coordinates can be shown at all time
   const contradict = ['brush', 'tooltip'];
 
-  if (contradict.includes(config.id) && config.enabled && !state.interactionConfig[config.id].enabled) {
+  if (
+    contradict.includes(config.id) &&
+    config.enabled &&
+    !state.interactionConfig[config.id].enabled
+  ) {
     // only enable one interaction at a time
     contradict.forEach(k => {
       if (k !== config.id) {
@@ -605,8 +618,8 @@ export const addFilterUpdater = (state, action) =>
  */
 export const toggleFilterAnimationUpdater = (state, action) => ({
   ...state,
-  filters: state.filters.map(
-    (f, i) => (i === action.idx ? {...f, isAnimating: !f.isAnimating} : f)
+  filters: state.filters.map((f, i) =>
+    i === action.idx ? {...f, isAnimating: !f.isAnimating} : f
   )
 });
 
@@ -622,10 +635,52 @@ export const toggleFilterAnimationUpdater = (state, action) => ({
  */
 export const updateAnimationSpeedUpdater = (state, action) => ({
   ...state,
-  filters: state.filters.map(
-    (f, i) => (i === action.idx ? {...f, speed: action.speed} : f)
+  filters: state.filters.map((f, i) =>
+    i === action.idx ? {...f, speed: action.speed} : f
   )
 });
+
+/**
+ * Update animation current time
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @param {Number} action.value current time value of action
+ * @returns {Object} nextState
+ * @public
+ *
+ */
+
+export const playAnimationUpdater = (state, {value}) => ({
+  ...state,
+  animationConfig: {
+    ...state.animationConfig,
+    currentTime: value
+  }
+});
+
+/**
+ * Enable animation domain with the min and max of timestamps from geojson
+ * @memberof visStateUpdaters
+ * @param {Object} state `visState`
+ * @param {Object} action action
+ * @returns {Object} nextState
+ * @public
+ *
+ */
+
+export const enableLayerAnimationUpdater = (state, action) => {
+  const {oldLayer} = action;
+  const [minTs, maxTs] = getTimeAnimationDomain(oldLayer, state.datasets);
+  return {
+    ...state,
+    animationConfig: {
+      ...state.animationConfig,
+      currentTime: minTs,
+      domain: [minTs, maxTs]
+    }
+  };
+};
 
 /**
  * Show larger time filter at bottom for time playback (apply to time filter only)
@@ -853,7 +908,7 @@ export const showDatasetTableUpdater = (state, action) => {
  * @returns {Object} nextState
  * @public
  */
-export const resetMapConfigUpdater = (state) => ({
+export const resetMapConfigUpdater = state => ({
   ...INITIAL_VIS_STATE,
   ...state.initialState,
   initialState: state.initialState
@@ -921,10 +976,12 @@ export const layerHoverUpdater = (state, action) => ({
  */
 export const layerClickUpdater = (state, action) => ({
   ...state,
-  mousePos: state.interactionConfig.coordinate.enabled ? {
-    ...state.mousePos,
-    pinned: state.mousePos.pinned ? null : cloneDeep(state.mousePos)
-  } : state.mousePos,
+  mousePos: state.interactionConfig.coordinate.enabled
+    ? {
+        ...state.mousePos,
+        pinned: state.mousePos.pinned ? null : cloneDeep(state.mousePos)
+      }
+    : state.mousePos,
   clicked: action.info && action.info.picked ? action.info : null
 });
 
@@ -935,15 +992,14 @@ export const layerClickUpdater = (state, action) => ({
  * @returns {Object} nextState
  * @public
  */
-export const mapClickUpdater = (state) => {
+export const mapClickUpdater = state => {
   return {
-  ...state,
-  clicked: null
-}
+    ...state,
+    clicked: null
+  };
 };
 
 export const mouseMoveUpdater = (state, {evt}) => {
-
   if (Object.values(state.interactionConfig).some(config => config.enabled)) {
     return {
       ...state,
@@ -956,7 +1012,7 @@ export const mouseMoveUpdater = (state, {evt}) => {
   }
 
   return state;
-}
+};
 /**
  * Toggle visibility of a layer for a split map
  * @memberof visStateUpdaters
@@ -991,14 +1047,18 @@ export const toggleLayerForMapUpdater = (state, {mapIndex, layerId}) => {
 
   return {
     ...state,
-    splitMaps: splitMaps.map((sm, i) => i === mapIndex ? {
-      ...splitMaps[i],
-      layers: {
-        ...splitMaps[i].layers,
-        // if layerId not in layers, set it to visible
-        [layerId]: !splitMaps[i].layers[layerId]
-      }
-    } : sm)
+    splitMaps: splitMaps.map((sm, i) =>
+      i === mapIndex
+        ? {
+            ...splitMaps[i],
+            layers: {
+              ...splitMaps[i].layers,
+              // if layerId not in layers, set it to visible
+              [layerId]: !splitMaps[i].layers[layerId]
+            }
+          }
+        : sm
+    )
   };
 };
 
@@ -1037,7 +1097,6 @@ export const updateVisDataUpdater = (state, action) => {
     }),
     {}
   );
-
   if (!Object.keys(newDateEntries).length) {
     return state;
   }
@@ -1124,11 +1183,12 @@ function closeSpecificMapAtIndex(state, action) {
 
   // update layer visibility
   const newLayers = layers.map(layer =>
-    !mapLayers[layer.id] && layer.config.isVisible ?
-      layer.updateLayerConfig({
-        // if layer.id is not in mapLayers, it should be inVisible
-        isVisible: false
-      }) : layer
+    !mapLayers[layer.id] && layer.config.isVisible
+      ? layer.updateLayerConfig({
+          // if layer.id is not in mapLayers, it should be inVisible
+          isVisible: false
+        })
+      : layer
   );
 
   // delete map
@@ -1157,16 +1217,19 @@ export const loadFilesUpdater = (state, action) => {
   const loadFileTasks = [
     Task.all(filesToLoad.map(LOAD_FILE_TASK)).bimap(
       results => {
-        const data = results.reduce((f, c) => ({
-          // using concat here because the current datasets could be an array or a single item
-          datasets: f.datasets.concat(c.datasets),
-          // we need to deep merge this thing unless we find a better solution
-          // this case will only happen if we allow to load multiple keplergl json files
-          config: {
-            ...f.config,
-            ...(c.config || {})
-          }
-        }), {datasets: [], config: {}, options: {centerMap: true}});
+        const data = results.reduce(
+          (f, c) => ({
+            // using concat here because the current datasets could be an array or a single item
+            datasets: f.datasets.concat(c.datasets),
+            // we need to deep merge this thing unless we find a better solution
+            // this case will only happen if we allow to load multiple keplergl json files
+            config: {
+              ...f.config,
+              ...(c.config || {})
+            }
+          }),
+          {datasets: [], config: {}, options: {centerMap: true}}
+        );
         return addDataToMap(data);
       },
       error => loadFilesErr(error)
@@ -1212,6 +1275,7 @@ export function addDefaultLayers(state, datasets) {
     ],
     []
   );
+
   return {
     ...state,
     layers: [...state.layers, ...defaultLayers],